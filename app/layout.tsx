c1743a4d04c3998c0ae967fc2d260b03c9c0de60
Scott Gillis <scott.gillis@paragon-inc.com>
2025-10-14 16:25:51 -0400

    lytics build issue resolution

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @next/next/no-page-custom-font */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+
 import { Geist, Geist_Mono } from "next/font/google";
 
 import '../styles/third-party.css';
@@ -37,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
+       
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
         <link
@@ -68,16 +70,6 @@ export default function RootLayout({
         <link href='/favicon.ico' rel='icon' type='image/ico' sizes='16x16' />
         <link rel='apple-touch-icon' href='/path/to/apple-touch-icon.png' />
         <meta name='theme-color' content='#317EFB' />
-
-        
-        <script type="text/javascript">
-        !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
-        jstag.init({
-          src: 'https://c.lytics.io/api/tag/afc2d72afcd3efde452150d105c25dbb/latest.min.js'
-        });
-        
-        jstag.pageView();
-        </script>
 
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
